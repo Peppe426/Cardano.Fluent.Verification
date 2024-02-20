ce59013f8523e97dd4ea5f22410b462abe20c31a
Peter Liljeholm <peter.liljeholm@cardiolex.se>
2024-02-20 07:39:16 +0100

    feat(acceptance test): added Accesscontrol

---
+++ Src/Fluent.Verification/Fluent.Verification.AcceptanceTests/Features/AccessControl.feature
@@ -0,0 +1,10 @@
+﻿Feature: Access Control
+Showcase a user-centric perspective, allowing individuals to configure and personalize their account security, integrate hardware for authentication, manage cryptographic credentials, and set specific access permissions, all facilitated by the Access Control Programmer. The actors in these scenarios include users, hardware devices (such as tokens), cryptographic keys, and system administrators focused on compliance and security.
+
+
+@AccessControlSettings
+Scenario: User customizing their access control settings
+	Given a user is concerned about securing their account
+	When the user accesses the Access Control Programmer feature
+	Then they define their personalized access control settings
+	And they enable two-factor authentication for enhanced security
---
+++ Src/Fluent.Verification/Fluent.Verification.AcceptanceTests/StepDefinitions/AccessControlStepDefinitions.cs
@@ -0,0 +1,30 @@
+﻿namespace Fluent.Verification.AcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public sealed class AccessControlStepDefinitions
+    {
+        [Given(@"a user is concerned about securing their account")]
+        public void GivenAUserIsConcernedAboutSecuringTheirAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [When(@"the user accesses the Access Control Programmer feature")]
+        public void WhenTheUserAccessesTheAccessControlProgrammerFeature()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Then(@"they define their personalized access control settings")]
+        public void ThenTheyDefineTheirPersonalizedAccessControlSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Then(@"they enable two-factor authentication for enhanced security")]
+        public void ThenTheyEnableTwoFactorAuthenticationForEnhancedSecurity()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
